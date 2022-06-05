5ba8eb91f96c0df15faf041a0ab8d3d183666591
Камиля <vildanova-kamilya99@yandex.ru>
2022-06-05 21:54:54 +0400

    Added README.md, other files

--- src/test/java/com/vildanova/tests/SecondTests.java
+++ src/test/java/com/vildanova/tests/SecondTests.java
@@ -1,24 +1,36 @@
 package com.vildanova.tests;
 
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
 
 import static com.vildanova.constant.Constant.url.SECOND_HOME_PAGE;
 
-@Test
-public class SecondTests extends TestBase{
+public class SecondTests extends TestBase {
 
-        public void secondTest() {
+    @Test
+    @Feature("Pdp")
+    @Story("Test number two")
+    @Owner("Vildanova Kamilya")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link(name = "www.litres.ru", url = "https://www.litres.ru")
+    @Description("Комментарий ко 2 тесту")
+    public void secondTest() {
 
-            basePage.openSite(SECOND_HOME_PAGE);
-            mainPage.selectMenu();
-            menuPage.selectSelection();
-            anotherThemePage.selectButton();
-            anotherThemePage.selectThemeOfMenu();
-            anotherThemePage.selectBook();
-            takeSubscriptionPage.takeSubscription();
-            takeSubscriptionPage.buyBook();
-            takeSubscriptionPage.switchTo();
-            assertPage.checkNameOfButton();
-            assertPage.checkTitle();
+        basePage.openSite(SECOND_HOME_PAGE);
+
+        mainPage.selectMenu();
+        menuPage.selectSubTopic();
+        anotherThemePage
+                .selectButton()
+                .selectThemeOfMenu()
+                .selectBook();
+        takeSubscriptionPage
+                .takeSubscription()
+                .buyBook()
+                .switchTo();
+
+        assertPage
+                .checkNameOfButton()
+                .checkTitle();
     }
 }
